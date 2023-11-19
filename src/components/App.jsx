b2bbf5652890370ac6cb46695ee630a087f33322
Serhii Filatov <filser.spirit@gmail.com>
2023-11-19 15:24:20 +0200

    refactor: app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,71 +13,74 @@ import { PER_PAGE } from '../utils/searchService';
 
 export class App extends Component {
   state = {
-    query: '',
-    page: 1,
     images: [],
     isloading: false,
-    error: false,
+    searchValue: '',
+    currentPage: 1,
     loadMore: false,
     isOpenModal: false,
     modalData: null,
   };
 
-  handleSearch = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.currentPage !== this.state.currentPage ||
+      prevState.searchValue !== this.state.searchValue
+    ) {
+      this.getImages();
+    }
+  }
+
+  getImages = async () => {
+    try {
+      this.setState({ isloading: true });
+      const { searchValue, currentPage } = this.state;
+      const response = await searchService(searchValue, currentPage);
+
+      const newImages = response.hits.map(
+        ({ id, webformatURL, largeImageURL, tags }) => ({
+          id,
+          webformatURL,
+          largeImageURL,
+          tags,
+        })
+      );
+      this.setState(prevState => ({
+        images: [...(prevState.images || []), ...newImages],
+      }));
+
+      if (response.totalHits !== 0 && this.state.currentPage === 1) {
+        toast.success(`Hooray! We found ${response.totalHits} images!`);
+      }
+      const totalPage = Math.ceil(response.totalHits / PER_PAGE);
+
+      if (totalPage > currentPage) {
+        this.setState({ loadMore: true });
+      } else if (totalPage === currentPage && response.totalHits) {
+        toast.error("Sorry, but you've reached the end of search results.");
+        this.setState({ loadMore: false });
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      this.setState({ isloading: false });
+    }
+  };
+
+  handleSearch = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
     const query = form.search.value.trim().toLowerCase();
     if (query === '') {
       toast.error('Sorry, there are no images matching your search query.');
       return;
     }
-    this.setState({ query: query, page: 1, images: null });
+    this.setState({ searchValue: query, currentPage: 1, images: null });
     form.reset();
   };
 
-  async componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      try {
-        this.setState({ isloading: true });
-        const { query, page } = this.state;
-        const resp = await searchService(query, page);
-
-        const newImages = resp.hits.map(
-          ({ id, webformatURL, largeImageURL, tags }) => ({
-            id,
-            webformatURL,
-            largeImageURL,
-            tags,
-          })
-        );
-        this.setState(prevState => ({
-          images: [...(prevState.images || []), ...newImages],
-        }));
-
-        if (resp.totalHits !== 0 && this.state.page === 1) {
-          toast.success(`Hooray! We found ${resp.totalHits} images!`);
-        }
-        const totalPage = Math.ceil(resp.totalHits / PER_PAGE);
-
-        if (totalPage > page) {
-          this.setState({ loadMore: true });
-        } else if (totalPage === page && resp.totalHits) {
-          toast.error("Sorry, but you've reached the end of search results.");
-          this.setState({ loadMore: false });
-        }
-      } catch (error) {
-        toast.error(error.message);
-      } finally {
-        this.setState({ isloading: false });
-      }
-    }
-  }
-
   handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+    this.setState(prevState => ({ currentPage: prevState.currentPage + 1 }));
   };
 
   openModal = someDataToModal => {
